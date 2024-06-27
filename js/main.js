dce18297b1932d3be54b9094f6a4b1093711a489
sined27 <133235199+sined277@users.noreply.github.com>
2024-06-27 11:18:29 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const burgerButton = document.querySelector(".menu-btn")
 const burgerList = document.querySelector(".navigation-header")
 
-
 burgerButton.addEventListener("click", () => {
     burgerList.classList.toggle("_active")
     burgerButton.classList.toggle("_active")
